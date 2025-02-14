f31a3ed0705afc6eba257ba55ad85fa41fb51e87
bohaos <bohaos@outlook.com>
2025-02-14 18:47:46 +0800

    feat: update credit upgrade deploy script

--- scripts/upgrade/credit.js
+++ scripts/upgrade/credit.js
@@ -7,13 +7,14 @@ async function main() {
     let nonce = await ethers.provider.getTransactionCount(deployer.address);
     console.log("Nonce:", nonce);
 
-    const usdt = "Zero";
-    console.log("usdt:", usdt);
+    // const usdt = ethers.ZeroAddress;
+    const ERC20Mock = await ethers.getContractFactory("ERC20Mock");
+    const usdt = await ERC20Mock.deploy("Mock USDT", "USDT", deployer.address, ethers.parseUnits("999999999999999", 18));
+    console.log("Usdt deployed to:", usdt.target);
 
     // Get the contract factory
     const TokenCreditUpgradeable = await ethers.getContractFactory("TokenCreditUpgradeable");
-
-    const tokenCredit = await upgrades.deployProxy(TokenCreditUpgradeable, [usdt]);
+    const tokenCredit = await upgrades.deployProxy(TokenCreditUpgradeable, [usdt.target]);
     console.log("TokenCredit deployed to:", tokenCredit.target);
     console.log("Finish token credit contract deployment");
 }
