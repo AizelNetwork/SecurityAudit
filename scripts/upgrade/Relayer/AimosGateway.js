9cdad8bb452e67cd3490c07793ce72f63f69f2c6
bohaos <bohaos@outlook.com>
2025-05-29 10:39:19 +0800

    feat: update aimos gateway scripts

--- scripts/upgrade/Relayer/AimosGateway.js
+++ scripts/upgrade/Relayer/AimosGateway.js
@@ -4,20 +4,38 @@ async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Using account:", deployer.address);
 
+    const feeCoins = [ethers.ZeroAddress];
+    const feeAmounts = [ethers.parseEther("0.01")];
+    console.log("feeCoins:", feeCoins);
+    console.log("feeAmounts:", feeAmounts);
+
+    const FeeManager = await ethers.getContractFactory("FeeManager");
+    const feeManager = await FeeManager.deploy(
+        feeCoins,
+        feeAmounts,
+    );
+    console.log("FeeManager deployed to:", feeManager.target);
+
     const models = [
-        "peaq_model_filter_tinybert.tar.gz",
-        "peaq_model_filter.tar.gz",
-        "Llama-3.3-70B-Instruct-Q8_0.gguf",
-        "combinder_model.tar.gz",
-        "eth_predict.tar.gz",
-        "llama-3.2-3b-instruct-q4_k_m.gguf",
-        "llama-3.2-1b-instruct-q8_0.gguf",
-        "Transfer-Agent"
+        "DeepSeek-R1-Distill-Llama-8B-Q4_K_M",
+        "DeepSeek-R1-Distill-Qwen-14B-Q4_K_M",
+        "DeepSeek-R1-Distill-Qwen-32B-Q4_K_M",
+        "Qwen3-1.7B-Q4_K_M",
+        "Qwen3-14B-Q4_K_M",
+        "Qwen3-32B-Q4_K_M",
+        "Qwen3-8B-Q4_K_M",
+        "gemma-3-12b-it-qat-Q4_K_M",
+        "Llama-4-Scout-17B-16E-Instruct-Q4_K_M",
+        "peaq-content-filter",
     ];
 
-    const AimosGateway = await ethers.getContractFactory("AimosGateway");
-    const aimosGateway = await AimosGateway.deploy(deployer.address, models);
-    console.log("AimosGateway deployed to:", aimosGateway.target);
+    // const AimosGateway = await ethers.getContractFactory("AimosGateway");
+    // const aimosGateway = await AimosGateway.deploy(deployer.address, feeManager.target, models);
+    // console.log("AimosGateway deployed to:", aimosGateway.target);
+
+    const AimosGatewayUpgradeable = await ethers.getContractFactory("AimosGatewayUpgradeable");
+    const aimosGatewayUpgradeable = await upgrades.deployProxy(AimosGatewayUpgradeable, [deployer.address, deployer.address, feeManager.target, models]);
+    console.log("AimosGatewayUpgradeable deployed to:", aimosGatewayUpgradeable.target);
 }
 
 main()
